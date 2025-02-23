e73ac869574a98e46a0311f612d053886cc9caef
Qamir alii <a.bitpins@gmail.com>
2025-02-23 11:50:25 +0330

    update list2

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { SupportFixComponent } from '../support-fix/support-fix.component';
 import { FooterComponent } from "../footer/footer.component";
+import { HeaderComponent } from "../header/header.component";
 
 
 @Component({
   selector: 'app-body',
-  imports: [SupportFixComponent, FooterComponent],
+  imports: [SupportFixComponent, FooterComponent, HeaderComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.scss'
 })
