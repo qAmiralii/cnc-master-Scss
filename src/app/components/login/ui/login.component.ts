c0a1676b9f317310f17fbf5716bba473e5494ac6
qamiralii <a.bitpins@gmail.com>
2025-04-19 21:58:55 +0330

    fixing login_4

--- src/app/components/login/ui/login.component.ts
+++ src/app/components/login/ui/login.component.ts
@@ -11,6 +11,9 @@ import { map, shareReplay } from 'rxjs/operators';
 import {MatCardModule} from '@angular/material/card';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { Login } from '../models/login';
 
 @Component({
   selector: 'app-login',
@@ -24,12 +27,24 @@ import {MatInputModule} from '@angular/material/input';
     MatIconModule,
     AsyncPipe,
     MatCardModule,
+    MatCheckboxModule,
+    FormsModule,
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  Login:Login = {
+    username: '',
+    password: '',
+    keepMe: false
+  }
+  isValid(){
+    if (this.Login.username.trim()=='' || this.Login.password=='') {
+      return true;
+    }
+    return false;
+  }
 
   private breakpointObserver = inject(BreakpointObserver);
 
