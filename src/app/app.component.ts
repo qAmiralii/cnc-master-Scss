81ab241240bcbd99e1e8906182b5f22f26c47bb3
qamiralii <a.bitpins@gmail.com>
2025-04-29 23:22:24 +0330

    flowbite fixed :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/ui/header.component';
 import 'flowbite';
+import { Component, inject, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'cncMaster_sCSS';
+  constructor(private router: Router) { }
+  // router = @inject(Router);     چرا این کار نمی کند
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        // Initialize Flowbite پس از هر نویگیت
+        initFlowbite();
+      }
+    });
+  }
 }
